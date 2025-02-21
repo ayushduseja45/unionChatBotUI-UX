46dc2c4e3ceb1b9006f6b387824ece7849b25b30
ayushduseja45 <dusejaayush2910@gmail.com>
2025-02-21 11:50:09 +0530

    logo added to login

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -27,8 +27,10 @@ const Login = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="flex justify-center">
-          <Bank className="w-16 h-16 text-blue-900" />
+          {/* <Bank className="w-16 h-16 text-blue-900" /> */}
+          <img src='logo.png' className='w-20 h-20'/>
         </div>
+
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Welcome to Vyom AI
         </h2>
