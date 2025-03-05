d3524c38914c5e6585d6a8efb08609d099caa732
ayushduseja45 <dusejaayush2910@gmail.com>
2025-03-06 00:38:09 +0530

    pata nhiii

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0fdb3f6667"
+    "revision": "0.8k7vln5r0h8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -114,7 +114,7 @@ const Login = () => {
           ) : (
             <div className="text-center">
               <button
-                onClick={() => navigate('/verify/face')}
+                onClick={() => navigate('/verify/video')}
                 className="w-full flex items-center justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-900 hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 Start Face Recognition
